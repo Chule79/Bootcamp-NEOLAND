85107d592c2bf2cb8a317297e18ef11054177f8b
Chule79 <jsantoslopez99@gmail.com>
2022-09-27 17:43:40 +0200

    fear: Iteración #2: Buscar la palabra más larga

--- Javascript/ejercicios/neoland-basic-functions/app.js
+++ Javascript/ejercicios/neoland-basic-functions/app.js
@@ -10,4 +10,25 @@
 //------------------------------------------------------------------------------
 
 //Iteración #2: Buscar la palabra más larga
+    const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+    let longWord = "";
+    function findLongestWord(param) {
+      for (i = 0; i < param.length; i++){
+        if(longWord.length < param[i].length){
+            longWord = param[i];
+        }
+      }
+        console.log(longWord);
+    }
 
+    findLongestWord(avengers);
+
+//------------------------------------------------------------------------------------
+
+//Iteración #3: Calcular la suma
+
+    const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+    function sumAll(param) {
+    
+    }
