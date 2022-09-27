d1fac21dd9e22201e495a822fb26ffd0649e129c
Chule79 <jsantoslopez99@gmail.com>
2022-09-27 17:15:42 +0200

    fear: Funciones Buscar el maximo

---
+++ Javascript/ejercicios/neoland-basic-functions/app.js
@@ -0,0 +1,13 @@
+//Iteración #1: Buscar el máximo  
+   function sum(numberOne , numberTwo) {
+        if (numberOne > numberTwo){
+            console.log(numberOne);
+        } else {console.log(numberTwo);}
+    }
+
+    sum(12, 57);
+
+//------------------------------------------------------------------------------
+
+//Iteración #2: Buscar la palabra más larga
+
