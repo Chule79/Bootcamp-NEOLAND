5604765a6ba4cb8be3e3bcca401c2e9aaefc07b3
Chule79 <jsantoslopez99@gmail.com>
2022-09-28 20:06:59 +0200

    Fear: Iteration 2 Loops

--- EJERCICIOS/Javascript/neoland-loops/app.js
+++ EJERCICIOS/Javascript/neoland-loops/app.js
@@ -11,3 +11,20 @@
 
 //Iteración #2: Condicionales avanzados
 
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+alumns.forEach((alumns) => {
+    if (alumns.T1 == true && alumns.T2 == true || alumns.T1 == true && alumns.T3 == true || alumns.T2 == true && alumns.T3 == true) {
+            alumns.isApproved = true; 
+    }else {alumns.isApproved = false;  }
+});
+
+console.log(alumns);
+
+//------------------------------------------------------------------------------------------
