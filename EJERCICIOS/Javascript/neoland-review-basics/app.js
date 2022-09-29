1e74602112bf0920c3b302abc1fa2961daeb10c3
Chule79 <jsantoslopez99@gmail.com>
2022-09-29 16:44:22 +0200

    Fear: Iteracion 3 de reviews el 2 no funciona

--- EJERCICIOS/Javascript/neoland-review-basics/app.js
+++ EJERCICIOS/Javascript/neoland-review-basics/app.js
@@ -19,4 +19,85 @@ console.log(categorias);
 //---------------------------------------------------------------------------------------------------------
 
 //Iteración #2: Mix Fors
+//NO SALE
+    const users = [
+        {name: 'Manolo el del bombo',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 50},
+                rain: {format: 'ogg', volume: 60},
+                firecamp: {format: 'mp3', volume: 80},
+            }
+        },
+        {name: 'Mortadelo',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 30},
+                shower: {format: 'ogg', volume: 55},
+                train: {format: 'mp3', volume: 60},
+            }
+        },
+        {name: 'Super Lopez',
+            favoritesSounds: {
+                shower: {format: 'mp3', volume: 50},
+                train: {format: 'ogg', volume: 60},
+                firecamp: {format: 'mp3', volume: 80},
+            }
+        },
+        {name: 'El culebra',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 67},
+                wind: {format: 'ogg', volume: 35},
+                firecamp: {format: 'mp3', volume: 60},
+            }
+        },
+    ]
 
+  
+
+//---------------------------------------------------------------------------------------
+
+//Iteración #3: Mix Fors
+
+    const users2 = [
+        {name: 'Manolo el del bombo',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 50},
+                rain: {format: 'ogg', volume: 60},
+                firecamp: {format: 'mp3', volume: 80},
+            }
+        },
+        {name: 'Mortadelo',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 30},
+                shower: {format: 'ogg', volume: 55},
+                train: {format: 'mp3', volume: 60},
+            }
+        },
+        {name: 'Super Lopez',
+            favoritesSounds: {
+                shower: {format: 'mp3', volume: 50},
+                train: {format: 'ogg', volume: 60},
+                firecamp: {format: 'mp3', volume: 80},
+            }
+        },
+        {name: 'El culebra',
+            favoritesSounds: {
+                waves: {format: 'mp3', volume: 67},
+                wind: {format: 'ogg', volume: 35},
+                firecamp: {format: 'mp3', volume: 60},
+            }
+        },
+    ]
+    
+    let conteo = []
+
+    for (const user2 of users2) {
+        for (const tipos2 in user2.favoritesSounds) {
+            if (conteo.includes(tipos2)) {
+                conteo.push(tipos2)                   
+            }
+            conteo[tipos2] = conteo[tipos2] + 1 || 1    
+         }
+     }
+     console.log(conteo);
+
+//-------------------------------------------------------------------------------------------------------------
