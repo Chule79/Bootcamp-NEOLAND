e5c84202e69f813719a8dae1b220d5fa60d11e44
Chule79 <jsantoslopez99@gmail.com>
2022-09-29 14:23:46 +0200

    Fear: Iteracion 4 y 5 de loops

--- EJERCICIOS/Javascript/neoland-loops/app.js
+++ EJERCICIOS/Javascript/neoland-loops/app.js
@@ -28,3 +28,25 @@ alumns.forEach((alumns) => {
 console.log(alumns);
 
 //------------------------------------------------------------------------------------------
+
+//Iteración #3: Probando For...of
+
+    const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+    for (const place of placesToTravel) {
+        console.log(place);
+    }
+
+//--------------------------------------------------------------------------------------------
+
+//Iteración #4: Probando For...in
+    const alien = {
+        name: 'Wormuck',
+        race: 'Cucusumusu',
+        planet: 'Eden',
+        weight: '259kg'
+    }
+
+    for (const key in alien) {
+       console.log(`${key}: ${alien[key]}`)
+    }
