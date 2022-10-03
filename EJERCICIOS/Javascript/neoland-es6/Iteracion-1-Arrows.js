6185213c5d084cbf909a9626ef3cad69cc75eb21
Chule79 <jsantoslopez99@gmail.com>
2022-10-03 16:48:36 +0200

    iteracion1 y 2 terminadas

---
+++ EJERCICIOS/Javascript/neoland-es6/Iteracion-1-Arrows.js
@@ -0,0 +1,17 @@
+const arrow = (a = 10, b = 5) => {
+    return a + b
+};
+
+
+
+
+
+
+
+
+//1.1 Ejecuta esta función sin pasar ningún parametro
+    console.log(arrow());
+//1.2 Ejecuta esta función pasando un solo parametro
+    console.log(arrow(4));
+//1.3 Ejecuta esta función pasando dos parametros
+console.log(arrow(4, 12));
