0063de4aa2158ef328851efcf1cab38760f6705d
Chule79 <jsantoslopez99@gmail.com>
2022-10-03 16:05:21 +0200

    Terminados los eventos de javascrip

--- EJERCICIOS/Javascript/neoland-events/app.js
+++ EJERCICIOS/Javascript/neoland-events/app.js
@@ -1,5 +1,18 @@
 const btn = document.querySelector('#btnToClick');
-const focus = document.querySelector('#focus');
+const foc = document.querySelector('.focus');
+const inp = document.querySelector('.value')
 
+//1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
+//evento click que ejecute un console log con la información del evento del click
 
 btn.addEventListener("click", () => {console.log("has echo click")});
+
+//1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
+
+foc.addEventListener("focus", () => {console.log(foc.value)});
+
+//1.3 Añade un evento 'input' que ejecute un console.log con el valor del input
+
+inp.addEventListener("input", () => {console.log(inp.value);})
+
+
